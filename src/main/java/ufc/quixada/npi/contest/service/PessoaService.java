4347bd5ce59c5b81974992f052a889dac8b98173
adanbueno <adan_bueno@hotmail.com>
2022-02-02 18:14:12 -0300

    fix:refactoring-sugery

--- src/main/java/ufc/quixada/npi/contest/service/PessoaService.java
+++ src/main/java/ufc/quixada/npi/contest/service/PessoaService.java
@@ -7,6 +7,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import ufc.quixada.npi.contest.model.Pessoa;
 import ufc.quixada.npi.contest.repository.PessoaRepository;
+import ufc.quixada.npi.contest.util.GetPessoa;
 import ufc.quixada.npi.contest.validator.ContestException;
 
 import java.util.List;
@@ -24,13 +25,22 @@ public class PessoaService {
 	private MessageService messsagemService;
 	
 	public void addOrUpdate(Pessoa pessoa) throws ContestException {
-		List<Pessoa> pessoas = pessoaRepository.findByCpfOrEmailAndIdIsNot(pessoa.getCpf(), pessoa.getEmail(), pessoa.getId() == null ? 0 : pessoa.getId());
+		Long idPessoa = GetPessoa.getId(pessoa);
+		List<Pessoa> pessoas = pessoaRepository.findByCpfOrEmailAndIdIsNot(getCpf(pessoa), getEmail(pessoa), idPessoa == null ? 0 : idPessoa);
 
 		if(!pessoas.isEmpty()) {
 			throw new ContestException(messsagemService.getMessage("ERRO_USUARIO_EXISTENTE"));
 		}
 
 		pessoaRepository.save(pessoa);
+	}
+	
+	private String getCpf(Pessoa pessoa) {
+		return pessoa.getCpf();
+	}
+	
+	private String getEmail(Pessoa pessoa) {
+		return pessoa.getCpf();
 	}
 
 	public List<Pessoa> list() {
